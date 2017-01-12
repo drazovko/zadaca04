074032283179885476e5c856a29fc9ce76155adc
juradrugiPosao <drazovko@foi.hr>
2017-01-12 12:16:30 +0100

    Popis svih aktivnih korisnika
    Skripta popis_korisnika.php generira popis svih aktivnih korisnika iz 
    tablice korisnici. 
    
    Zadatak 4: PHP datoteku (popis_korisnika.php) koja dinamički kreira 
    popis svih aktiviranih korisnika iz odgovarajuće tablice (min 3 aktivna 
    korisnika moraju postojati u bazi), formatirajući popis kao listu 
    korisničkih imena, i za svaki element te liste nalazi se nova lista 
    koja sadrži ime, prezime i grad. Svako korisničko ime je hiperveza na 
    PHP skriptu detalji_korisnika.php.

--- popis_korisnika.php
+++ popis_korisnika.php
@@ -1,13 +1,80 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>Početna stranica</title>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width">
+        <meta name="application-name" content="popis korisnika">
+        <meta name="author" content="Dragan Zovko">
+        <meta name="description" content="datum_kreiranja: 12.1.2017.">
+        <link rel="stylesheet" type="text/css" href="css/drazovko.css"/>
+        <link rel="stylesheet" type="text/css" media="screen and (max-width: 450px)" href="css/drazovko_mobitel.css" />
+        <link rel="stylesheet" type="text/css" media="screen and (min-width:451px) and (max-width: 800px)" href="css/drazovko_tablet.css" />
+        <link rel="stylesheet" type="text/css" media="screen and (min-width:801px) and (max-width: 1000px)" href="css/drazovko_pc.css" />
+        <link rel="stylesheet" type="text/css" media="screen and (min-width:1001px)" href="css/drazovko_tv.css" />
     </head>
     <body>
-        <h2>Popis korisnika!!!</h2>
+        <header id="zaglvalje" >
+            
+            <img src="img/logo.png" alt="foi logo" id="logo" />
+            <p id="header_naslov">Zadaća 04</p>
+        </header>
+        <nav id="meni">
+            <ul>
+                <li><a href="index.html">Početna stranica</a></li>
+                <li><a href="osobna.html">O meni</a></li>
+                <li><a href="registracija.html">Registracija</a></li>
+                <li><a href="registracija_jquery.html">Registracija jquery</a></li>
+                <li><a href="prijava.html">Prijava</a></li>
+                <li><a href="korisnici.html">Korisnici</a></li>
+                <li><a href="korisnici_jquery.html">Korisnici jquery</a></li>
+                <li><a href="gen_korisnici_jquery.html">Korisnici gen. jquery</a></li>
+                <li><a href="http://www.foi.unizg.hr" target="_blank">Foi web</a></li>
+    
+            </ul>
+        </nav>
+        <section id="sadržaj">
+            <h1>Popis korisnika</h1>
+            <section id="uvod">
+            <h3 class="podnaslov">Popis aktiviranih korisnika:</h3>
+        
+            <ul>
         <?php
+        include 'baza.class.php';
+        
+        $dbc = new baza();
+        
+        $sql = "SELECT `kor_ime`, `ime`, `prezime`, `grad` FROM `korisnik` "
+                . "WHERE `aktiviran` = 1";
+        
+        $odgovor = $dbc->selectUpit($sql);
+        
+        while (list($korIme, $ime, $prezime, $grad) = $odgovor->fetch_array()) {
+            echo "<li><a href=\"detalji_korisnika.php?korIme=$korIme\">$korIme</a></li>";
+            echo "<ul>";
+            echo "<li>$ime</li>";
+            echo "<li>$prezime</li>";
+            echo "<li>$grad</li></br>";
+            echo "</ul>";
+        }
         
         ?>
+            </ul>
+            
+            </section>
+        </section>
+        <footer id="footer">
+            <address>
+                Kontakt:
+                <a href="mailto:drazovko@foi.hr">Dragan Zovko</a>
+                <p>Vrijeme potrebno za riješavanje aktivnog dokumenta ~ jedan sat</p>
+                <p>Vrijeme potrebno za riješavanje cijelokupnog riješenja ~ jedan dan</p>
+                <a href="http://validator.w3.org/check?uri=referer">
+                    <img src="http://4.bp.blogspot.com/-Cf1ZOzD0qJ4/VJ-gbgf4EMI/AAAAAAAAAUo/vZrtEJa_sBQ/s1600/W3C_HTML5_certified.png" alt="HTML validator" width="102" height="30"></a>
+                <a href="http://jigsaw.w3.org/css-validator/check/referer">
+                    <img src="http://3.bp.blogspot.com/-aZnVc1Yv54E/UVWLLkTNtmI/AAAAAAAAAEk/xwN9Vkkhofc/s1600/512px-Valid-css-v_(W3C_Markup_Validation_Service).svg.png" alt="css validator" width="102" height="30"></a>
+            
+            </address>
+        </footer>
     </body>
 </html>
