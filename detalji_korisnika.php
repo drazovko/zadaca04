25f8d7acf028f872497ec36ddb0ecf629f4b37c8
juradrugiPosao <drazovko@foi.hr>
2017-01-17 11:02:54 +0100

    Popis detalji korisnika - konačno završeno
    Skripta detalji_korisnika.php generira detaljan popis za odabranog 
    korisnika kao na obrascu za registraciju, konačno završeno
    
    Zadatak 4.5: PHP skriptu (detalji_korisnika.php) koja se učitava 
    aktiviranjem linka korisničko ime iz liste (definirane pod 4) koja 
    prikazuje sve podatke za odabranog korisnika iz liste u HTML obrascu
    (izgleda isto kao i obrazac za registraciju sa svim elementima samo što 
    se za sliku stvarno prikazuje slika – nije nužno da se radi upload 
    slike, može biti uvijek ista slika).

--- detalji_korisnika.php
+++ detalji_korisnika.php
@@ -32,8 +32,8 @@
         </nav>
         
         <section id="sadržaj"> 
-            <section id="uvod">
-            <ul>
+            
+            
             <?php
             include 'baza.class.php';
             
@@ -52,113 +52,56 @@
         
         $slika = "";
          ?>
-            <table id="glupa_tablica" >
-                <tr><td>Ime:</td><td><?php echo $ime?></td></tr>    
-                <tr><td>Prezime:</td><td><?php echo $prezime?></td></tr>
-                <tr><td>Slika:</td><td><img src="img/drazovko.jpg" alt="slika" width="50" height="50"></td></tr>
-                <tr><td>Adresa:</td><td><?php echo $adresa?></td></tr>
-                <tr><td>Županija:</td><td><?php echo $zupanija?></td></tr>
-                <tr><td>Grad:</td><td><?php echo $grad?></td></tr>
-                <tr><td>Email:</td><td><?php echo $email?></td></tr>
-                <tr><td>Korisničko ime:</td><td><?php echo $korIme?></td></tr>
-                <tr><td>Lozinka:</td><td><?php echo '**********'?></td></tr>
-                <tr><td>Telefon:</td><td><?php echo $telefon?></td></tr>
-                <tr><td>Datum rodjenja:</td><td><?php echo $datumRodjenja?></td></tr>
-                <tr><td>Spol:</td><td><?php echo $spol?></td></tr>
-                <tr><td>Pretplata na mail:</td><td>
-                    <?php 
+        <form action="http://arka.foi.hr/WebDiP/2014/materijali/zadace/ispis_forme.php" 
+                  method="post" name="registracija" id="obrazac" enctype="multipart/form-data" >
+                
+                <label for="ime">Ime: </label>
+                <input type="text" name="ime" id="ime" size="20" maxlength="30" 
+                       value="<?php echo $ime?>" disabled=""/><br />
+                <label for="prezime">Prezime: </label>
+                <input type="text" name="prezime" id="prezime" size="20" maxlength="50" 
+                       value="<?php echo $prezime?>" disabled=""/><br />
+                <label for="slikaJa">Slika: </label>
+                <img name="slikaJa" src="img/drazovko.jpg" alt="slika" width="50" height="50"><br />
+                <label for="adresa" >Adresa: </label>
+                <input name="adresa" id="adresa" rows="2" cols="30" value="<?php echo $adresa?>" disabled="" ><br />
+                <label for="županije">Županija: </label>
+                <input type="text" name="županije" id="županije" size="20" maxlength="50" 
+                       value="<?php echo $zupanija?>" disabled=""/><br />
+                
+                <label for="grad">Grad: </label>  
+                <input type="text" name="grad" id="grad" value="<?php echo $grad?>" disabled=""/><br />
+                
+                <label for="mail">Email: </label>
+                <input type="email" name="mail" id="mail" value="<?php echo $email?>" disabled="" /><br />
+                <label for="kor_ime">Korisničko ime: </label>
+                <input type="text" name="kor_ime" id="kor_ime" title="pet ili više znakova"
+                       value="<?php echo $korIme?>" disabled="" /><br />
+                
+                <label for="lozinka">Lozinka: </label>
+                <input type="password" name="lozinka" id="lozinka" title="šest ili više znakova"
+                       value="**********" disabled=""  /><br />
+                <label for="telefon">Telefon: </label>
+                <input type="tel" name="telefon" id="telefon" value="<?php echo $telefon?>" disabled=""
+                       title="Format: xxx xxxxxxx" /><br />
+                <label for="dat_rodj">Datum rođenja: </label>
+                <input type="date" name="dat_rodj" id="dat_rodj" value="<?php echo $datumRodjenja?>" disabled=""/><br />
+                
+                <label for="spol">Spol: </label>
+                <input type="text" name="spol" id="spol"  value="<?php echo $spol?>" disabled="">
+                    
+                <label for="pretplata">Pretplata na mail: </label>
+                <input type="text" name="pretplata" id="pretplata" value="<?php 
                     if($pretplataNaMail == 1){
                         echo "Da";
                     }  else {
                         echo "Ne";
                     }
-                    ?></td></tr>
-                
-                
-                
-            </table>
-            </section>
-            </section>
-        <section id="sadržaj">
-            <form action="http://arka.foi.hr/WebDiP/2014/materijali/zadace/ispis_forme.php" 
-                  method="post" name="registracija" id="obrazac" enctype="multipart/form-data" >
-                <div id="greske"></div>
-                <label for="ime">Unesite vaše ime: </label>
-                <input type="text" name="ime" id="ime" size="20" maxlength="30" 
-                       placeholder="  -- ime --" /><br />
-                <label for="prezime">Unesite vaše prezime: </label>
-                <input type="text" name="prezime" id="prezime" size="20" maxlength="50" 
-                       placeholder="  -- prezime --" /><br />
-                <label for="datoteka">Odaberite sliku: </label>
-                <input type="file" name="datoteka" id="datoteka" /><br />
-                <label for="adresa" >Adresa: </label>
-                <textarea name="adresa" id="adresa" rows="2" cols="30" ></textarea><br />
-                <label for="županije">Županija: </label>
-                <select id="županije">
-                    <option value="-1" >-- Odaberi županiju --</option>
-                    <optgroup label="Sjeverna Hrvatska">
-                        <option value="1" >BJELOVARSKO-BILOGORSKA</option>
-                        <option value="2" >KOPRIVNIČKO-KRIŽEVAČKA</option>
-                        <option value="3" >KRAPINSKO-ZAGORSKA</option>
-                        <option value="4" >MEĐIMURSKA</option>
-                        <option value="5" selected="selected">VARAŽDINSKA</option>
-                    </optgroup>
-                    <optgroup label="Središnja Hrvatska">
-                        <option value="5" >GRAD ZAGREB</option>
-                        <option value="6" >KARLOVAČKA</option>
-                        <option value="7" >SISAČKO-MOSLAVAČKA</option>
-                        <option value="8" >ZAGREBAČKA</option>
-                    </optgroup>
-                    <optgroup label="Zapadna Hrvatska">
-                        <option value="9" >ISTARSKA</option>
-                        <option value="10" >LIČKO-SENJSKA</option>
-                        <option value="11" >PRIMORSKO-GORANSKA</option>
-                    </optgroup>
-                    <optgroup label="Istočna Hrvatska">
-                        <option value="12" >BRODSKO-POSAVSKA</option>
-                        <option value="13" >OSJEČKO-BARANJSKA</option>
-                        <option value="14" >POŽEŠKO-SLAVONSKA</option>
-                        <option value="15" >VIROVITIČKO-PODRAVSKA</option>
-                        <option value="16" >VUKOVARSKO-SRIJEMSKA</option>
-                    </optgroup>
-                    <optgroup label="Južna Hrvatska">
-                        <option value="17" >DUBROVAČKO-NERETVANSKA</option>
-                        <option value="18" >SPLITSKO-DALMATINSKA</option>
-                        <option value="19" >ŠIBENSKO-KNINSKA</option>
-                        <option value="20" >ZADARSKA</option>
-                    </optgroup>
-                </select><br />
-                
-                <label for="grad">Grad: </label>  
-                <input type="text" name="grad" id="grad"/><br />
-                
-                <label for="mail">Email: </label>
-                <input type="email" name="mail" id="mail" 
-                       pattern="[a-zA-Z0-9]+[@]+foi+[.]+hr" required="" /><br />
-                <label for="kor_ime">Korisničko ime: </label>
-                <input type="text" name="kor_ime" id="kor_ime" title="pet ili više znakova"
-                       pattern=".{5,}" /><br />
-                
-                <label for="lozinka">Lozinka: </label>
-                <input type="password" name="lozinka" id="lozinka" title="šest ili više znakova"
-                       pattern=".{6,}"  /><br />
-                <label for="telefon">Telefon: </label>
-                <input type="tel" name="telefon" id="telefon" pattern="[0-9]{3}[/ ][0-9]{7}"
-                       title="Format: xxx xxxxxxx" /><br />
-                <label for="dat_rodj">Datum rođenja: </label>
-                <input type="date" name="dat_rodj" id="dat_rodj"/><br />
-                
-                <label for="spol">Spol: </label>
-                <input type="radio" name="spol" id="spol" class="točke" value="1">M
-                    <input type="radio" name="spol" id="spol1" class="točke" value="2">Ž
-                    <input type="radio" name="spol" id="spol2" class="točke" value="3" >Ne znam<br />
-                <label for="pretplata">Pretplata na mail: </label>
-                <input type="checkbox" name="pretplata" id="pretplata" value="da"/><br />    
-                <input name="registracija" type="submit" id="submit_btn" value="Slanje podataka" class="gumb" />
-                <input name="registracija" type="reset" value="Brisanje formulara" class="gumb" />
+                    ?>" disabled=""/><br />    
+
             </form>
-        
             </section>
+        
         <footer id="footer">
             <address>
                 Kontakt:
