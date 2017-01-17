e3e3338052352fc3e085b1e33d34f877bd5708bd
juradrugiPosao <drazovko@foi.hr>
2017-01-17 19:19:42 +0100

    Autorizacija - započeto
    Započete skripte index.php i prijava.php iz zadatka br. 5. točke 1. i 2.
    
    Zadatak 5.1. PHP datoteku index.php koja ukoliko je korisnik prijavljen
     prikazuje informacije iz zadaće 1. A ako korisnik nije prijavljen 
    sustav ga preusmjerava na skriptu za prijavu (opisanu pod 2).

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+//if (true) {
+  //  header("Location: prijava.php");
+    
+//}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Početna stranica</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <meta name="application-name" content="početna stranica">
+        <meta name="author" content="Dragan Zovko">
+        <meta name="description" content="datum_kreiranja: 17.1.2017.">
+        <link rel="stylesheet" type="text/css" href="css/drazovko.css"/>
+        <link rel="stylesheet" type="text/css" media="screen and (max-width: 450px)" href="css/drazovko_mobitel.css" />
+        <link rel="stylesheet" type="text/css" media="screen and (min-width:451px) and (max-width: 800px)" href="css/drazovko_tablet.css" />
+        <link rel="stylesheet" type="text/css" media="screen and (min-width:801px) and (max-width: 1000px)" href="css/drazovko_pc.css" />
+        <link rel="stylesheet" type="text/css" media="screen and (min-width:1001px)" href="css/drazovko_tv.css" />
+    </head>
+    <body>
+        <header id="zaglvalje" >
+            
+            <img src="img/logo.png" alt="foi logo" id="logo" />
+            <p id="header_naslov">Zadaća 05</p>
+        </header>
+        <nav id="meni">
+            <ul>
+                <li><a href="index.html">Početna stranica</a></li>
+                <li><a href="osobna.html">O meni</a></li>
+                <li><a href="registracija.html">Registracija</a></li>
+                <li><a href="registracija_jquery.html">Registracija jquery</a></li>
+                <li><a href="prijava.html">Prijava</a></li>
+                <li><a href="korisnici.html">Korisnici</a></li>
+                <li><a href="korisnici_jquery.html">Korisnici jquery</a></li>
+                <li><a href="gen_korisnici_jquery.html">Korisnici gen. jquery</a></li>
+                <li><a href="http://www.foi.unizg.hr" target="_blank">Foi web</a></li>
+    
+            </ul>
+        </nav>
+        <section id="sadržaj">
+            <h1>Web dizajn i programiranje</h1><br>
+            <section id="uvod">
+            <h2>Osnovne informacije</h2><br>
+            
+            <h3 class="podnaslov">Cilj kolegija:</h3>
+        
+            <p class = "tekst">
+                Cilj kolegija je upoznavanje studenata s elementima dizajna web stranica i razvoja web aplikacija. Predmetom se obrađuju glavni elementi koji čine pojedinačne sastavne komponente cjelovitog projektnog rješenja na web platformi. Predmet prati moguće razine realizacije Web projekata tako da se studentima pruža uvid u različite tehnološke mogućnosti koje mogu primijeniti u konkretnim situacijama. Studenti tijekom praktičnog dijela kolegija rade vježbe kojima postepeno razvijaju pojedine gradive blokove kasnijih web stranica i aplikacija. Prezentacijom izabranih rješenja otvara se diskusija tijekom koje studenti mogu izraziti svoje mišljenje o dizajnu, dovršenosti i drugim dogovorenim kriterijima kvalitete, čime se potiče kritičko razmišljanje o tuđem i vlastitom rješenju
+            </p>
+            <h3 class="podnaslov">Ishodi učenja predmeta</h3>
+            <ul>
+                <li>analizirati opterećenje Web poslužitelja i predložiti potrebne radnje za poboljšanje njegovih performansi</li>
+                <li>izraditi Web aplikacije različite namjene i složenosti ...</li>
+                <li>razlikovati Web tehnologije</li>
+                <li>razumijeti osobine skriptnih programskih jezika i koristiti ih u realizaciji Web aplikacije</li>
+                <li>razumjeti i koristiti razne servise za potrebe Web aplikacija</li>
+                <li>razumjeti i primijeniti hipertekstualno i hipermedijsko povezivanje dokumenata</li>
+                <li>razumjeti i primijeniti preporuke Web dizajna</li>
+                <li>razumjeti i primijeniti preporuke Web inženjerstva</li>
+                <li>razumjeti način funkcioniranja Web mjesta i Web aplikacija</li>
+                <li>razumjeti osobine korisničke i poslužiteljske strane Web aplikacija te ih primijeniti na način koji odgovara specifičnostima pojedinog projekta</li>
+                <li>razumjeti principe rada Web autorskih alata i znati ih koristiti ih u razvoju Web mjesta</li>
+            </ul>
+            </section>
+        </section>
+        <footer id="footer">
+            <address>
+                Kontakt:
+                <a href="mailto:drazovko@foi.hr">Dragan Zovko</a>
+                <p>Vrijeme potrebno za riješavanje aktivnog dokumenta ~ jedan sat</p>
+                <p>Vrijeme potrebno za riješavanje cijelokupnog riješenja ~ jedan dan</p>
+                <a href="http://validator.w3.org/check?uri=referer">
+                    <img src="http://4.bp.blogspot.com/-Cf1ZOzD0qJ4/VJ-gbgf4EMI/AAAAAAAAAUo/vZrtEJa_sBQ/s1600/W3C_HTML5_certified.png" alt="HTML validator" width="102" height="30"></a>
+                <a href="http://jigsaw.w3.org/css-validator/check/referer">
+                    <img src="http://3.bp.blogspot.com/-aZnVc1Yv54E/UVWLLkTNtmI/AAAAAAAAAEk/xwN9Vkkhofc/s1600/512px-Valid-css-v_(W3C_Markup_Validation_Service).svg.png" alt="css validator" width="102" height="30"></a>
+            
+            </address>
+        </footer>
+    </body>
+</html>
---
+++ prijava.php
@@ -0,0 +1,105 @@
+<?php
+include 'baza.class.php';
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $korIme = $_POST['kor_ime'];
+    $lozinka = $_POST['lozinka'];
+    $greske = "";
+    $brojac = 1 + $_POST['brojac'];
+    
+    
+    $dbc = new baza();
+    
+    $sql = "select kor_ime, ime, prezime, email from korisnik "
+            . "where kor_ime = '$korIme' and password = '$lozinka'";
+    
+    $rezultatUpita = $dbc->selectUpit($sql);
+    
+    $brojRedova = $rezultatUpita->num_rows;
+    if ($brojRedova ===1) {
+        $korisnikOK = TRUE;
+        
+        header("Location: index.php");
+    }  else {
+        $greske = "Neispravno korisničko ime ili lozinka!!!</br>";
+        if ($brojac > 1) {
+            $greske = $greske . "Pokušaj broj " . $brojac . "!</br>";
+        }
+    }
+}  else {
+    $greske = "";
+    $brojac = 1;
+}
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Prijava</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <meta name="application-name" content="prijava">
+        <meta name="author" content="Dragan Zovko">
+        <meta name="description" content="datum_kreiranja: 3.4.2015.">     
+        <link rel="stylesheet" type="text/css" href="css/drazovko.css"/>
+        <link rel="stylesheet" type="text/css" media="screen and (max-width: 450px)" href="css/drazovko_mobitel.css" />
+        <link rel="stylesheet" type="text/css" media="screen and (min-width:451px) and (max-width: 800px)" href="css/drazovko_tablet.css" />
+        <link rel="stylesheet" type="text/css" media="screen and (min-width:801px) and (max-width: 1000px)" href="css/drazovko_pc.css" />
+        <link rel="stylesheet" type="text/css" media="screen and (min-width:1001px)" href="css/drazovko_tv.css" />
+    </head>
+    <body>
+        <header id="zaglvalje">
+            <img src="img/logo.png" alt="foi logo" id="logo" />
+            <p id="header_naslov">Zadaća 03</p>
+        </header>
+        <nav id="meni">
+            <ul>
+                <li><a href="index.html">Početna stranica</a></li>
+                <li><a href="osobna.html">O meni</a></li>
+                <li><a href="registracija.html">Registracija</a></li>
+                <li><a href="prijava.html">Prijava</a></li>
+                <li><a href="korisnici.html">Korisnici</a></li>
+                <li><a href="http://www.foi.unizg.hr" target="_blank">Foi web</a></li>
+    
+            </ul>
+        </nav>
+        <section id="sadržaj">
+            <article>
+                <h2 class="tekst_na_prijavi">Pristup ovoj stranici omogućen je samo autoriziranim korisnicima.</h2>
+                <p class="tekst_na_prijavi">Ako imate pravo pristupa, moći ćete pristupiti ovoj stranici nakon što se prijavite.</p>
+                <form action="" method="post" name="prijava" id="prijava" enctype="multipart/form-data">
+                    <div id="greske"><?php echo $greske ?></div></br>
+                    <label for="kor_ime">Korisničko ime: </label>
+                    <input type="text" name="kor_ime" id="kor_ime" autofocus="" required=""
+                           placeholder="Korisničko ime"/><br />
+                    <label for="lozinka">Lozinka: </label>
+                    <input type="password" name="lozinka" id="lozinka" required=""
+                           placeholder="Lozinka"/><br />
+                    <label for="zapamti">Zapamti me: </label>
+                    
+                    <input type="checkbox" name="zapamti_me" id="zapamti" value="da"/><br />
+                    <button type="submit" form="prijava" value="submit" class="gumb">
+                        Prijavi se</button><br />
+                    <input type="text" name='brojac' id="brojac" value="<?php echo $brojac ?>" hidden="">
+                </form>
+            </article>
+            <article class="tekst_na_prijavi">
+                Registriraj se <a href="registracija.html">ovdje</a>
+            </article>
+        </section>
+        <footer id="footer">
+            <address>
+                Kontakt:
+                <a href="mailto:drazovko@foi.hr">Dragan Zovko</a>
+                <p>Vrijeme potrebno za riješavanje aktivnog dokumenta ~ jedan sat</p>
+                <p>Vrijeme potrebno za riješavanje cijelokupnog riješenja ~ jedan dan</p>
+                <a href="http://validator.w3.org/check?uri=referer">
+                    <img src="http://4.bp.blogspot.com/-Cf1ZOzD0qJ4/VJ-gbgf4EMI/AAAAAAAAAUo/vZrtEJa_sBQ/s1600/W3C_HTML5_certified.png" alt="HTML validator" width="102" height="30"></a>
+                <a href="http://jigsaw.w3.org/css-validator/check/referer">
+                    <img src="http://3.bp.blogspot.com/-aZnVc1Yv54E/UVWLLkTNtmI/AAAAAAAAAEk/xwN9Vkkhofc/s1600/512px-Valid-css-v_(W3C_Markup_Validation_Service).svg.png" alt="css validator" width="102" height="30"></a>
+            
+            </address>
+        </footer>
+        <script src="js/drazovko.js"></script>
+    </body>
+</html>
